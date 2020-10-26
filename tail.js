6a131fcd2853314abb70cd505ddcbeb80460130d
Areej Basalamah <areejbasalamah@cmail.carleton.ca>
2020-10-26 23:20:32 +0000

    completed assignemnt

---
+++ tail.js
@@ -0,0 +1,37 @@
+const assertEqual = function(actual, expected) {
+  const warnning = String.fromCodePoint('0x1F525');
+  const passed = String.fromCodePoint('0x1F389');
+  if (actual === expected) {
+    console.log(`${passed}${passed}${passed} Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`${warnning}${warnning}${warnning} Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const tail = function(arr) {
+  if (arr.length === 0) {
+    return [];
+  } else {
+    let newArr = arr.slice();
+    newArr.shift();
+    return newArr;
+  }
+};
+
+// TEST CODE
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+
+// Test Case: testing for empty and one element arrays
+const oneElement = tail(['h']);
+const emptyArr = tail([]);
+assertEqual(oneElement.length,0);
+assertEqual(emptyArr.length,0);
+
+// Test Case: Check the original array
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
