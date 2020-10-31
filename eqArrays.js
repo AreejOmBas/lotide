d0ae78f3e2c3f04d843998d97e6659042e686625
Areej Basalamah <areejbasalamah@cmail.carleton.ca>
2020-10-31 19:41:45 +0000

    separated test in different file

--- eqArrays.js
+++ eqArrays.js
@@ -1,13 +1,4 @@
-const assertEqual = function(actual, expected) {
-  const warnning = String.fromCodePoint('0x1F525');
-  const passed = String.fromCodePoint('0x1F389');
-  if (actual === expected) {
-    console.log(`${passed}${passed}${passed} Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`${warnning}${warnning}${warnning} Assertion Failed: ${actual} !== ${expected}`);
 
-  }
-};
 
 const eqArrays = function(arr1, arr2){
   if(arr1 === undefined || arr2 === undefined){
@@ -24,18 +15,8 @@ const eqArrays = function(arr1, arr2){
   }
 };
 
-// console.log(eqArrays([1, 2, 3], [3, 2, 1]) )// => false
-// console.log(eqArrays([1, 2, 3], [1, 2, 3]) )// => true
+module.exports = eqArrays;
 
-// console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"]) )// => true
-// console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
-// console.log(eqArrays([])) // => false
 
-console.log(assertEqual(eqArrays([1, 2, 3], [3, 2, 1]),false));
-console.log(assertEqual(eqArrays([3, 2, 1], [3, 2, 1]),true));
-console.log(assertEqual(eqArrays([]),false));
-console.log(assertEqual(eqArrays([45, 2, 3], [1, 2, 3]),false));
-console.log(assertEqual(eqArrays([1, 2], [3, 2, 1]),false));
-console.log(assertEqual(eqArrays([1, 2, 3, 4], [1, 2, 3, 4]),true));
 
 
