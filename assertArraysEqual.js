e8ae3c0d678a56bebe49a9b4daee5969dd99f815
Areej Basalamah <areejbasalamah@cmail.carleton.ca>
2020-10-31 19:44:58 +0000

    separated test in different file

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,17 +1,6 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1 === undefined || arr2 === undefined) {
-    return false;
-  } else if (arr1.length !== arr2.length) {
-    return false;
-  } else {
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]) {
-        return false;
-      }
-    }
-    return true;
-  }
-};
+const eqArrays = require('./eqArrays');
+
+
 
 const assertArraysEqual = function(arr1, arr2){
   const warnning = String.fromCodePoint('0x1F525');
@@ -24,5 +13,4 @@ const assertArraysEqual = function(arr1, arr2){
   }
 }
 
-assertArraysEqual([1,2,3],[1,2,3]);
-assertArraysEqual([1,2],[1]);
+module.exports = assertArraysEqual;
